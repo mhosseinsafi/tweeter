a77119fe738e7870b2226a45be7b7379d758cb62
Hossein <hossein.saafi@gmail.com>
2023-06-20 15:40:54 -0700

    Form Validation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,6 +5,7 @@
  */
 
 // Fake data taken from initial-tweets.json
+
 const data = [
   {
     "user": {
@@ -15,7 +16,7 @@ const data = [
     "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-    "created_at": 1461116232227
+    "created_at": 1544666010224
   },
   {
     "user": {
@@ -26,57 +27,81 @@ const data = [
     "content": {
       "text": "Je pense, donc je suis"
     },
-    "created_at": 1461113959088
+    "created_at": 1544666010225
   }
 ];
 
+
 $(document).ready(function() {
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+      console.log($tweet);
+    }
+  };
 
-const renderTweets = function(tweets) {
-  for (const tweet of tweets) {                    // loops through tweets
-    const $tweet = createTweetElement(tweet);     // calls createTweetElement for each tweet
-    $('#tweets-container').append($tweet);       // takes return value and appends it to the tweets container
-    console.log($tweet);
-  }
-};
+  $('form').submit(function(event) {
+    event.preventDefault(); // Prevent refresh behavior
 
-$('form').submit(function(event) {
-  event.preventDefault(); // Prevent the default form submission behavior
+    // Serialize the form data
+    const formData = $(this).serialize();
+    const tweetContent = $(this).find('textarea[name="text"]').val();
 
-  // Get the form data
-  const formData = $(this).serialize();
+    // Data validation
+    if (!tweetContent) {
+      alert('Tweet content cannot be empty');
+      return;
+    }
 
-  // Send an AJAX POST request to the server
-  $.ajax({
-    method: 'POST',
-    url: 'http://localhost:8080/tweets', 
-    data: formData
-  })
-  .then(function(response) {
-    // Handle the response from the server if needed
-    console.log('Tweet submitted successfully:', response);
-  })
+    if (tweetContent.length > 140) {
+      alert('Tweet content exceeds the maximum limit of 140 characters');
+      return;
+    }
+
+    // Send an AJAX POST request to the server
+    $.ajax({
+      method: 'POST',
+      url: 'http://localhost:8080/tweets',
+      data: formData           //Use the jQuery library to submit a POST request that sends the serialized data to the server
+    })
+    .then(function(response) {            // Handle the response from the server if needed
+      console.log('Tweet submitted successfully:', response);
+    });
+  });
+
+  const loadTweets = function() {
+    $.ajax({
+      method: 'GET',
+      url: 'http://localhost:8080/tweets',
+      dataType: 'json'
+    })
+    .then(function(response) {
+      console.log('Tweets loaded successfully:', response);
+      renderTweets(response);
+    });
+  };
+
+  loadTweets();          // Call the loadTweets function to load tweets on page load
+
+  const createTweetElement = function(tweet) {
+    const timePassed = timeago.format(tweet.created_at);
+    const $tweet = $(`
+      <article class="tweet">
+        <header>
+          <img src="${tweet.user.avatars}">
+          <h2>${tweet.user.name}</h2>
+          <span class="handle">${tweet.user.handle}</span>
+        </header>
+        <div class="content">
+          <p>${tweet.content.text}</p>
+        </div>
+        <footer>
+          <p>${timePassed}</p>
+        </footer>
+      </article>
+    `);
+
+    return $tweet;
+  };
 });
-
-const createTweetElement = function(tweet) {
-  const $tweet = $(`
-    <article class="tweet">
-      <header>
-        <img src="${tweet.user.avatars}">
-        <h2>${tweet.user.name}</h2>
-        <span class="handle">${tweet.user.handle}</span>
-      </header>
-      <div class="content">
-        <p>${tweet.content.text}</p>
-      </div>
-      <footer>
-        <p>${tweet.created_at}</p>
-      </footer>
-    </article>
-  `);
-
-  return $tweet;
-};
-
-renderTweets(data);
-})
